662dde7a46c4963400632b00d720e984b4013ee2
Abel3581 <marzoa3581@gmail.com>
2022-09-15 23:28:31 -0300

    agregado login

--- src/main/java/com/control/expensas/controller/AuthController.java
+++ src/main/java/com/control/expensas/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.control.expensas.controller;
 
+import com.control.expensas.model.dto.request.LoginRequest;
 import com.control.expensas.model.dto.request.UserRegisterRequest;
+import com.control.expensas.model.dto.response.LoginResponse;
 import com.control.expensas.model.dto.response.UserRegisterResponse;
 import com.control.expensas.service.abstraction.AuthService;
 import lombok.AllArgsConstructor;
@@ -24,5 +26,11 @@ public class AuthController {
     public ResponseEntity<UserRegisterResponse> register(@RequestBody @Valid  UserRegisterRequest request){
         UserRegisterResponse response = authService.register(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request){
+        LoginResponse response = authService.login(request);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- src/main/java/com/control/expensas/mapper/UserMapper.java
+++ src/main/java/com/control/expensas/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.control.expensas.mapper;
 
 import com.control.expensas.model.User;
 import com.control.expensas.model.dto.request.UserRegisterRequest;
+import com.control.expensas.model.dto.response.LoginResponse;
 import com.control.expensas.model.dto.response.UserRegisterResponse;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,12 @@ public class UserMapper {
     public UserRegisterResponse dtoToEntity(User user) {
         return UserRegisterResponse.builder()
                 .id(user.getUserId())
+                .email(user.getEmail())
+                .build();
+    }
+
+    public LoginResponse dtoLoginResponse(User user) {
+        return LoginResponse.builder()
                 .email(user.getEmail())
                 .build();
     }
